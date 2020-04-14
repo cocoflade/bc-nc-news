a8fa20782605ba403507ad6d4d4b60eca6cab1ba
Luke Flannery <lukeflannery@Lukes-MBP-2.broadband>
2020-04-14 15:03:20 +0100

    formatDates complete using TDD

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,5 +1,13 @@
-exports.formatDates = list => {};
+exports.formatDates = (list) => {
+  const correctDateFormat = list.map((input) => {
+    let date = new Date(input.created_at);
+    const { created_at, ...restOfKeys } = input;
+    return { created_at: date, ...restOfKeys };
+  });
 
-exports.makeRefObj = list => {};
+  return correctDateFormat;
+};
+
+exports.makeRefObj = (list) => {};
 
 exports.formatComments = (comments, articleRef) => {};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,101 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
   formatComments,
-} = require('../db/utils/utils');
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  it("will return an empty array when given an empty array", () => {
+    expect(formatDates([])).to.eql([]);
+  });
+  it("returns a new array with the correctly formatted timestamp", () => {
+    const input = [
+      {
+        title: "title-one",
+        topic: "mitch",
+        author: "james",
+        body: "This article",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+    ];
+    const expected = [
+      {
+        title: "title-one",
+        topic: "mitch",
+        author: "james",
+        body: "This article",
+        created_at: new Date(1542284514171),
+        votes: 100,
+      },
+    ];
+    expect(formatDates(input)).to.eql(expected);
+  });
+  it("does not mutate the original array", () => {
+    const input = [
+      {
+        title: "title-one",
+        topic: "mitch",
+        author: "james",
+        body: "This article",
+        created_at: 1542284514171,
+        votes: 100,
+      },
+    ];
+    const expected = [
+      {
+        title: "title-one",
+        topic: "mitch",
+        author: "james",
+        body: "This article",
+        created_at: new Date(1542284514171),
+        votes: 100,
+      },
+    ];
+    expect(formatDates(input)).to.not.equal(expected);
+  });
+  it("returns a new array with the correctly formatted timestamp on multiple objects", () => {
+    const input = [
+      {
+        title: "title-one",
+        topic: "mitch",
+        author: "james",
+        body: "This article",
+        created_at: 1416140514171,
+        votes: 100,
+      },
+      {
+        title: "title-two",
+        topic: "webb",
+        author: "john",
+        body: "This article",
+        created_at: 1416140514171,
+        votes: 90,
+      },
+    ];
+    const expected = [
+      {
+        title: "title-one",
+        topic: "mitch",
+        author: "james",
+        body: "This article",
+        created_at: new Date(1416140514171),
+        votes: 100,
+      },
+      {
+        title: "title-two",
+        topic: "webb",
+        author: "john",
+        body: "This article",
+        created_at: new Date(1416140514171),
+        votes: 90,
+      },
+    ];
+    expect(formatDates(input)).to.eql(expected);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
